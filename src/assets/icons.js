84a54d4f971556a226a8691e8211c023e3e2d788
yaztak <yaztak@gmail.com>
2024-12-03 21:34:58 +0330

    feat(component): Playback functionality for the new template and icons map
    Implemented the playback functionality
    Added an icons map

---
+++ src/assets/icons.js
@@ -0,0 +1,22 @@
+export const icons = new Map([
+  [
+    'play-outline',
+    `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -960 960 960' width='1000px' fill='#e8eaed'>
+      <path d='M360-272.31v-415.38L686.15-480 360-272.31ZM400-480Zm0 134 211.54-134L400-614v268Z'/>
+    </svg>`,
+  ],
+  [
+    'fast-forward-outline',
+    `<svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'>
+        <path
+          d='M163.85-295.38v-369.24L440.77-480 163.85-295.38Zm355.38 0v-369.24L796.15-480 519.23-295.38ZM203.85-480Zm355.38 0ZM203.85-370l165.23-110-165.23-110v220Zm355.38 0 165.23-110-165.23-110v220Z'/>
+      </svg>`,
+  ],
+  [
+    'fast-rewind-outline',
+    `<svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'>
+        <path
+          d='M796.15-295.38 519.23-480l276.92-184.62v369.24Zm-355.38 0L163.85-480l276.92-184.62v369.24Zm-40-184.62Zm355.38 0ZM400.77-370v-220L235.54-480l165.23 110Zm355.38 0v-220L590.92-480l165.23 110Z'/>
+      </svg>`,
+  ],
+])
--- src/audio-player.js
+++ src/audio-player.js
@@ -1,84 +1,110 @@
+import { icons } from './assets/icons.js'
+
 export default class AudioPlayer {
-    constructor(selector, options) {
-        this.audioElement = document.querySelector(selector);
-        this.audioSelector = selector;
-        this.wrapperClasses = [...this.audioElement.classList];
-        this.options = options;
-        this.url = this.getAudioUrl()
+  constructor(selector, options) {
+    this.audioSelector = selector;
+    this.options = options;
+    this.audioElement = document.querySelector(selector);
+    this.wrapperClasses = [...this.audioElement.classList];
+    this.url = this.getAudioUrl()
 
-        // Check if the selector exists and is an audio element
-        if (!this.audioElement || this.audioElement.tagName !== 'AUDIO') {
-            throw new Error('Invalid audio element selector.');
-        }
-
-        // Create the audio player UI
-        this.createAudioPlayerUI();
+    // Check if the selector exists and is an audio element
+    if (!this.audioElement || this.audioElement.tagName !== 'AUDIO') {
+      throw new Error('Invalid audio element selector.');
     }
 
-    getAudioUrl() {
-        // Check if the 'src' attribute is set
-        let audioUrl = this.audioElement.src;
+    // Create the audio player UI
+    this.createAudioPlayerUI();
+  }
 
-        // If 'src' is not set, check for <source> elements
-        if (!audioUrl) {
-            const sourceElements = this.audioElement.querySelectorAll('source');
+  getAudioUrl() {
+    // Check if the 'src' attribute is set
+    let audioUrl = this.audioElement.src;
 
-            // Use the first <source> element's 'src' attribute
-            if (sourceElements.length > 0) {
-                audioUrl = sourceElements[0].src;
-            }
-        }
+    // If 'src' is not set, check for <source> elements
+    if (!audioUrl) {
+      const sourceElements = this.audioElement.querySelectorAll('source');
 
-        // If no URL is found, return null or handle it as needed
-        return audioUrl || null;
+      // Use the first <source> element's 'src' attribute
+      if (sourceElements.length > 0) {
+        audioUrl = sourceElements[0].src;
+      }
     }
 
-    getClassNames() {
-        // Needs to be converted to array
-        console.log(this.wrapperClasses)
-        return this.wrapperClasses.join(' ')
-    }
+    // If no URL is found, return null or handle it as needed
+    return audioUrl || null;
+  }
 
-    getClassSelectors() {
-        let classes = [];
-        this.wrapperClasses.forEach(className => {
-            let selector = `.${className}`;
-            classes.push(selector);
-        })
-        return classes.join('');
-    }
+  getClassNames() {
+    // Needs to be converted to array
+    console.log(this.wrapperClasses)
+    return this.wrapperClasses.join(' ')
+  }
 
-    createAudioPlayerUI() {
-        const template = `
-      <div class="audio-player ${this.getClassNames()}">
-        <button class="play-button">Play</button>
-        <button class="pause-button">Pause</button>
-        <input type="range" class="volume-control" />
-        <!-- Add other UI elements as needed -->
-      </div>
+  getClassSelectors() {
+    let classes = [];
+    this.wrapperClasses.forEach(className => {
+      let selector = `.${className}`;
+      classes.push(selector);
+    })
+    return classes.join('');
+  }
+
+  getIcon(name) {
+    return icons.get(name)
+  }
+
+  createAudioPlayerUI() {
+    const template = `
+      <div class="audio-player__wrapper ${this.getClassNames()}" style="background-image: url('../src/assets/placeholder.svg')">
+  <img src="../src/assets/placeholder.svg" class="audio-player__image"/>
+  <div class="audio-player__label">
+    <span class="audio-player__title">Title</span>
+    <span class="audio-player__subtitle">Subtitle</span>
+  </div>
+  <div class="audio-player__progress">
+    <div class="audio-player__line">
+      <span class="audio-player__bullet"></span>
+    </div>
+    <div class="audio-player__time-wrapper">
+      <span class="audio-player__time">00:00</span>
+      <span class="audio-player__time">00:00</span>
+    </div>
+  </div>
+  <div class="audio-player__controls">
+    <button class="audio-player__btn">
+      ${this.getIcon('fast-rewind-outline')}
+    </button>
+    <button class="audio-player__btn audio-player__btn--play">
+      ${this.getIcon('play-outline')}
+    </button>
+    <button class="audio-player__btn">
+      ${this.getIcon('fast-forward-outline')}
+    </button>
+  </div>
+</div>
     `;
 
-        this.audioElement.insertAdjacentHTML('afterend', template);
-        this.audioElement.remove()
-        this.addEventListeners();
-    }
+    this.audioElement.insertAdjacentHTML('afterend', template);
+    this.audioElement.remove()
+    this.addEventListeners();
+  }
 
-    addEventListeners() {
-        // Add event listeners to your custom UI elements
-        const playButton = document.querySelector(`.audio-player${this.getClassSelectors()} .play-button`);
-        const pauseButton = document.querySelector(`.audio-player${this.getClassSelectors()}  .pause-button`);
-        console.log(`.audio-player${this.getClassSelectors()} .play-button`)
-        console.log(playButton)
+  addEventListeners() {
+    // Add event listeners to your custom UI elements
+    const playButton = document.querySelector(`.audio-player${this.getClassSelectors()} .audio-player__btn--play`);
 
-        playButton.addEventListener('click', () => this.play());
-        pauseButton.addEventListener('click', () => this.pause());
-    }
+    playButton.addEventListener('click', () => {
+      if (this.audioElement.paused) this.pause()
+      else this.play()
+    });
+  }
 
-    play() {
-        this.audioElement.play();
-    }
+  play() {
+    this.audioElement.play();
+  }
 
-    pause() {
-        this.audioElement.pause();
-    }
+  pause() {
+    this.audioElement.pause();
+  }
 }
